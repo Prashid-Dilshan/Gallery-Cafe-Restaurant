59ef3475ace7602d1e82b01cbf8d4f22bbcb6722
PRASHID <prashiddilshan0710@gmail.com>
2024-09-24 12:02:13 +0530

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+/**
+ * header sticky 
+ */
+
+const header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 100) {
+    header.classList.add("active");  
+  } else {
+    header.classList.remove("active");
+  }
+});
